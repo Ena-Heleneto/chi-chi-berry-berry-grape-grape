8b98d3d021b294aa24b7b22542a4b40a00249652
Ena-Heleneto <by15242952083@outlook.com>
2025-08-13 19:26:11 +0800

    重构项目结构，删除不必要的组件和文件，添加中间件支持，更新依赖项

---
+++ src/middleware/auth.ts
@@ -0,0 +1,8 @@
+import { defineMiddleware } from '@uni-helper/vite-plugin-uni-middleware/runtime'
+
+export default defineMiddleware((_to, _from) => {
+  // const store = useStore()
+  // if (!store.isLogin) {
+  //   return '/pages/login/index'
+  // }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import UniHelperComponents from '@uni-helper/vite-plugin-uni-components'
 import { WotResolver } from '@uni-helper/vite-plugin-uni-components/resolvers'
 import UniHelperLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import UniHelperManifest from '@uni-helper/vite-plugin-uni-manifest'
+import UniMiddleware from '@uni-helper/vite-plugin-uni-middleware'
 import UniHelperPages from '@uni-helper/vite-plugin-uni-pages'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -15,9 +16,7 @@ export default defineConfig({
     // https://github.com/uni-helper/vite-plugin-uni-manifest
     UniHelperManifest(),
     // https://github.com/uni-helper/vite-plugin-uni-pages
-    UniHelperPages({
-      dts: 'src/uni-pages.d.ts',
-    }),
+    UniHelperPages({ dts: 'src/uni-pages.d.ts' }),
     // https://github.com/uni-helper/vite-plugin-uni-layouts
     UniHelperLayouts(),
     // https://github.com/uni-helper/vite-plugin-uni-components
@@ -28,6 +27,7 @@ export default defineConfig({
     }),
     // https://github.com/uni-helper/plugin-uni
     Uni(),
+    UniMiddleware(),
     UniPolyfill(),
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
